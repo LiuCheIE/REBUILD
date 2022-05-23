4916f9fdaf7b5946d7aa7641253f999d9377118e
Che Liu <36301116+LiuCheIE@users.noreply.github.com>
2022-05-23 19:51:57 +0100

    Update config_CDTC.py

--- config_para/config_CDTC.py
+++ config_para/config_CDTC.py
@@ -1,5 +1,5 @@
-username = 'C##LPIS_CDTC'
-password = 'LPIS_CDTC'
-dsn = 'sdbahgeo3.agriculture.gov.ie/geo3.agriculture.gov.ie'
+username = ''
+password = ''
+dsn = ''
 port = 1521
 encoding = 'UTF-8'
