6666f5760ecdbd3353f8b7b9b713df04396f70c4
Che Liu <36301116+LiuCheIE@users.noreply.github.com>
2022-05-23 15:21:12 +0100

    Delete config.py

--- config.py
+++
@@ -1,4 +0,0 @@
-ConStr2Orcl = "DATA SOURCE=sdbahgeo3.agriculture.gov.ie:1521/geo3.agriculture.gov.ie;PASSWORD=icon_data;USER ID=C##LPIS_TRANSFORM"
-ConStr2OrclPOC = "DATA SOURCE=sdbahgeo3.agriculture.gov.ie:1521/geo3.agriculture.gov.ie;PASSWORD=lpispoc;USER ID=C##POC"
-ConStr2OrclCDTC = "DATA SOURCE=sdbahgeo3.agriculture.gov.ie:1521/geo3.agriculture.gov.ie;PASSWORD=LPIS_CDTC;USER ID=C##LPIS_CDTC"
-ConStr2OrclARCH  = "DATA SOURCE=sdbahgeo3.agriculture.gov.ie:1521/geo3.agriculture.gov.ie;PASSWORD=icon_data;USER ID=C##LPIS_ARCHIVE"
