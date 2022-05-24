08100200d08d28274d38d6bfdc3b8793bc458feb
Che Liu <36301116+LiuCheIE@users.noreply.github.com>
2022-05-24 11:45:30 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -38,7 +38,7 @@ if __name__ == '__main__':
 		db.check_fme_run(910)
 
 		db.flog("PRE VCLEAN - START")
-		db.fmerun("//sdbahgeo2/GISDEV/TRANSFORMED_PARCELS/FME_LIVE/FULL/Process/3_PYTHON\Python_Scripts\VClean3_PRE_BAT.bat")
+		db.fmerun("//sdbahgeo2/GISDEV/TRANSFORMED_PARCELS/FME_LIVE/FULL/Process/3_PYTHON/Python_Scripts/VClean3_PRE_BAT.bat")
 		db.flog("PRE VCLEAN - COMPLETE")
 
 		db.flog("START SLEEP")
@@ -49,7 +49,7 @@ if __name__ == '__main__':
 		db.check_fme_run(950)
 
 		db.create_forestry_view("V_TRANSFORM_FORESTRY")
-
+		#
 		db.fmerun("//sdbahgeo2/GISDEV/TRANSFORMED_PARCELS/FME_LIVE/FULL/Process/1_DISSOLVE_FOREST_SHELLS.bat")
 		db.check_fme_run(1)
 
@@ -94,13 +94,175 @@ if __name__ == '__main__':
 		db.add_delivered("TDLP_PARC_REPAIRED4", "TDLP_PARCELS_TEST9E")
 		db.flog("92_INSERT_DELIVERED_INCL_WS_INTO_PARCS - Complete")
 
+		db.add_to_test9("TDLP_PARCELS_TEST4", "TDLP_PARCELS_TEST9")
+
+		db.replace_duplicate_ws("TDLP_PARCELS_TEST9", "GEOM_2157")
+		db.recombine_split_polys("TDLP_PARCELS_TEST9")
+		db.flog("Q_REVALIDATE - COMPLETE")
+
+		db.fmerun("//sdbahgeo2/GISDEV/TRANSFORMED_PARCELS/FME_LIVE/FULL/Process/R_DEAGG.bat")
+		db.flog("R_DEAGG -    Complete")
+		db.check_fme_run(25)
+
+		db.replace_duplicate_ws("TDLP_PARCELS_TEST9A", "GEOM_2157")
+		db.flog("S_REMOVE_DUPL - COMPLETE")
+
+		db.fmerun("//sdbahgeo2/GISDEV/TRANSFORMED_PARCELS/FME_LIVE/FULL/Process/T_REMOVE_WS_OLAPS.bat")
+		db.flog("T_REMOVE_WS_OLAPS -    Complete")
+		db.check_fme_run(27)
+
+		db.fmerun("//sdbahgeo2/GISDEV/TRANSFORMED_PARCELS/FME_LIVE/FULL/Process/V_FIND_MULTI_DEAGG.bat")
+		db.flog("V_FIND_MULTI_DEAGG -    Complete")
+		db.check_fme_run(29)
+
+		db.replace_duplicate_ws("TDLP_PARCELS_TEST9C", "GEOM_2157")
+		db.flog("W_REMOVE_DUPL - COMPLETE")
+
+		db.skip_table("TDLP_PARCELS_TEST9C", "TDLP_PARCELS_TEST9E")
+
+		db.update_md_non_active("TDLP_PARCELS_TEST9E")
+		db.update_fad_gross_non_active("TDLP_PARCELS_TEST9E")
+
+		db.fmerun("//sdbahgeo2/GISDEV/TRANSFORMED_PARCELS/FME_LIVE/FULL/Process/CH55_NO_GAPS_TOWNLAND_FME.bat")
+		db.flog("CH55_NO_GAPS_TOWNLAND_FME -    Complete")
+		db.check_fme_run(701)
+
+		db.fmerun("//sdbahgeo2/GISDEV/TRANSFORMED_PARCELS/FME_LIVE/FULL/Process/CH55_NO_GAPS_DED_FME_FIX_TEMP.bat")
+		db.flog("CH55_NO_GAPS_DED_FME_FIX_TEMP -    Complete")
+		db.check_fme_run(703)
+
+		db.fmerun("//sdbahgeo2/GISDEV/TRANSFORMED_PARCELS/FME_LIVE/FULL/Process/Z4_SNAP_WS_TO_WEEKC.bat")
+		db.flog("Z4_SNAP_WS_TO_WEEKC")
+
+		db.test9e_from9e1("TDLP_PARCELS_TEST9E")
+		db.flog("test9efrom9e1")
+
+		db.fmerun("//sdbahgeo2/GISDEV/TRANSFORMED_PARCELS/FME_LIVE/FULL/Process/CH55_NO_GAPS_TOWNLAND_FME.bat")
+		db.flog("CH55_NO_GAPS_TOWNLAND_FME -    Complete")
+		db.check_fme_run(701)
+
+		db.fmerun("//sdbahgeo2/GISDEV/TRANSFORMED_PARCELS/FME_LIVE/FULL/Process/CH55_NO_GAPS_DED_FME_FIX_TEMP.bat")
+		db.flog("CH55_NO_GAPS_DED_FME_FIX_TEMP -    Complete")
+		db.check_fme_run(703)
+
+		db.fmerun("//sdbahgeo2/GISDEV/TRANSFORMED_PARCELS/FME_LIVE/FULL/Process/Z3_A_REMOVE_TWNLAND_GAPS_WS.bat")
+		db.flog("Z3_A_REMOVE_TWNLAND_GAPS_WS")
+
+		db.test9e_from9e1("TDLP_PARCELS_TEST9E")
+		db.flog("test9efrom9e1")
+
+		db.fmerun("//sdbahgeo2/GISDEV/TRANSFORMED_PARCELS/FME_LIVE/FULL/Process/Z4_Z1_ORACLE_TO_TEST9E_SHP_PLUS_WEEKC2.bat")
+		db.flog("Z4_Z1_ORACLE_TO_TEST9E_SHP_PLUS_WEEKC2")
+
+		db.fmerun("//sdbahgeo2/GISDEV/TRANSFORMED_PARCELS/FME_LIVE/FULL/Process/Z5_FILL_GAPS_TEST9ESHP_TO_ORCL.bat")
+		db.flog("Z5_FILL_GAPS_TEST9ESHP_TO_ORCL")
+
+		db.test9e_from9e1("TDLP_PARCELS_TEST9E")
+		db.flog("test9efrom9e1")
+
+		db.replace_duplicate_ws_only("TDLP_PARCELS_TEST9E")
+
+		db.fmerun("//sdbahgeo2/GISDEV/TRANSFORMED_PARCELS/FME_LIVE/FULL/Process/CH55_NO_GAPS_TOWNLAND_FME.bat")
+		db.flog("CH55_NO_GAPS_TOWNLAND_FME -    Complete")
+		db.check_fme_run(701)
+
+		db.fmerun("//sdbahgeo2/GISDEV/TRANSFORMED_PARCELS/FME_LIVE/FULL/Process/CH55_NO_GAPS_DED_FME_FIX_TEMP.bat")
+		db.flog("CH55_NO_GAPS_DED_FME_FIX_TEMP -    Complete")
+		db.check_fme_run(703)
+
+		db.insert_ws()     # 491 rows in VB.net
+
+		db.fmerun("//sdbahgeo2/GISDEV/TRANSFORMED_PARCELS/FME_LIVE/FULL/Process/CH55_NO_GAPS_TOWNLAND_FME.bat")
+		db.flog("CH55_NO_GAPS_TOWNLAND_FME -    Complete")
+		db.check_fme_run(701)
+
+		db.fmerun("//sdbahgeo2/GISDEV/TRANSFORMED_PARCELS/FME_LIVE/FULL/Process/CH55_NO_GAPS_DED_FME_FIX_TEMP.bat")
+		db.flog("CH55_NO_GAPS_DED_FME_FIX_TEMP -    Complete")
+		db.check_fme_run(703)
+
+		db.replace_duplicate_ws("TDLP_PARCELS_TEST9C", "GEOM_2157")
+		db.flog("replaceDuplicateWS + ORACLEGEOMVALIDATION")
+
+		db.fmerun("//sdbahgeo2/GISDEV/TRANSFORMED_PARCELS/FME_LIVE/FULL/Process/Z4_Z1_ORACLE_TO_TEST9E_SHP_PLUS_WEEKC2.bat")
+		db.flog("Z4_Z1_ORACLE_TO_TEST9E_SHP_PLUS_WEEKC2")
+
+		db.fmerun("//sdbahgeo2/GISDEV/TRANSFORMED_PARCELS/FME_LIVE/FULL/Process/Z5_SUBFEAT_OLAPS_NEW.bat")
+		db.flog("Z5_SUBFEAT_OLAPS_NEW -     Complete")
+
+		db.fmerun("//sdbahgeo2/GISDEV/TRANSFORMED_PARCELS/FME_LIVE/FULL/Process/Z5_SUBFEAT_OLAPS_NEW.bat")
+		db.flog("Z5_SUBFEAT_OLAPS_NEW -     Complete")
+		db.check_fme_run(51)
+
+		db.fmerun("//sdbahgeo2/GISDEV/TRANSFORMED_PARCELS/FME_LIVE/FULL/Process/Z6_EXCL_SLIVER_DETECT_ERAD2a.bat")
+		db.flog("Z6_EXCL_SLIVER_DETECT_ERAD2a -    Complete")
+		db.check_fme_run(52)
+
+		db.fmerun("//sdbahgeo2/GISDEV/TRANSFORMED_PARCELS/FME_LIVE/FULL/Process/Z7_EXPORT_FOR_SNAP.bat")
+		db.flog("Z7_EXPORT_FOR_SNAP -    Complete")
+		db.check_fme_run(53)
+
+		db.fmerun("//sdbahgeo2/GISDEV/TRANSFORMED_PARCELS/FME_LIVE/FULL/Process/Z7_SNAP_TO_PARCS_FME.bat")
+		db.flog("Z7_SNAP_TO_PARCS_FME -    Complete")
+		db.check_fme_run(54)
+
+		db.fmerun("//sdbahgeo2/GISDEV/TRANSFORMED_PARCELS/FME_LIVE/FULL/Process/3_PYTHON/Python_Scripts/VClean_SF_BAT.bat")
+		db.flog("VCLEAN_SF - COMPLETE")
+
+		db.flog("START SLEEP")
+		time.sleep(120)
+		db.flog("END SLEEP")
+
+		db.fmerun("//sdbahgeo2/GISDEV/TRANSFORMED_PARCELS/FME_LIVE/FULL/Process/Z8__VCLEAN_SF_SHP_ORCL.bat")
+		db.flog("Z8__VCLEAN_SF_SHP_ORCL -    Complete")
+		db.check_fme_run(56)
+
+		db.validate_subfeatures_oracle("TDLP_SUB_FEAT_REPAIRED_2")
+		db.flog("Z81_ORACLE_VALIDATION_SF - COMPLETE")
+
+		db.delete_efa_subfeatures("TDLP_SUB_FEAT_REPAIRED_2")
+		db.flog("Z82_DELETE EFA_SF_TYPE_33_34_35 - COMPLETE")
+
+		db.fmerun("//sdbahgeo2/GISDEV/TRANSFORMED_PARCELS/FME_LIVE/FULL/Process/Z84_RECLIP_SUBFEAT.bat")
+		db.flog("Z84_RECLIP_SUBFEAT -    Complete")
+		db.check_fme_run(59)
+
+		db.validate_subfeatures_oracle("TDLP_SUB_FEAT_REPAIRED_3")
+		db.flog("Z86_FINAL FIXES_SF - A - COMPLETE")
+
+		db.replace_duplicate_subfeatures("TDLP_SUB_FEAT_REPAIRED_3")
+		db.flog("Z86_FINAL FIXES_SF - B - COMPLETE")
+
+		db.update_area_fenced_off("TDLP_SUB_FEAT_REPAIRED_3")
+
+		db.fmerun("//sdbahgeo2/GISDEV/TRANSFORMED_PARCELS/FME_LIVE/FULL/Process/Z87_REMOVE THOSE IN WHITESP.bat")
+		db.flog("Z87_REMOVE THOSE IN WHITESP -    Complete")
+		db.check_fme_run(63)
+
+		db.delete_sf_from_whitespace("TDLP_SUB_FEAT_REPAIRED_3")
+		db.flog("Z88_DELETE_SF_IN_WHITESP - COMPLETE")
+
+		db.fmerun("//sdbahgeo2/GISDEV/TRANSFORMED_PARCELS/FME_LIVE/FULL/Process/Z89_CREATE_TBL_FORESTRY_WEEKC_IN_V_TRANSFORM_FOR.bat")
+		db.flog("Z89_CREATE_TBL_FORESTRY_WEEKC_IN_V_TRANSFORM_FOR -    Complete")
+		db.check_fme_run(65)
+
+		db.fmerun("//sdbahgeo2/GISDEV/TRANSFORMED_PARCELS/FME_LIVE/FULL/Process/Z891_CREATE_TBL_FORESTRY_SEHLL_WEEKC_IN_POC_SHELL_FOR.bat")
+		db.flog("Z891_CREATE_TBL_FORESTRY_SEHLL_WEEKC_IN_POC_SHELL_FOR -    Complete")
+		db.check_fme_run(66)
+
+		db.fmerun("//sdbahgeo2/GISDEV/TRANSFORMED_PARCELS/FME_LIVE/FULL/Process/Z911_CLEAN_SPIKE_ISSUE.bat")
+		db.flog("Z911_CLEAN_SPIKE_ISSUE -    Complete")
+
+		db.test9e_from9e1("TDLP_PARCELS_TEST9E")
+		db.flog("test9efrom9e1")
+
+
 		if db.connection:
 			db.connection.close()
 			db.connection_cdtc.close()
 			db.connection_poc.close()
 			db.connection_arch.close()
 			db.connection_weekc.close()
-			db.flog("-----------------------DONE--------------------------")
-			print("-----------------------DONE--------------------------")
+			db.flog("--------------------------DONE--------------------------")
+			print("--------------------------DONE--------------------------")
 	except cx_Oracle.Error as error:
 		print(error)
